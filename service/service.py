24c9abb4a377c664883c9430b85e825b4465363f
Timur Samkharadze <timur.samkharadze@sysco.no>
2019-10-28 13:28:53 +0100

    added new endpoint to consume messages from PubSub topics

--- service/service.py
+++ service/service.py
@@ -17,14 +17,20 @@ FAIL_ON_ERROR = str_to_bool(os.environ.get('FILE_ON_ERROR', "True"))
 
 THREAD_POOL_SIZE = int(os.environ.get('THREAD_POOL_SIZE', '10'))
 
+SUBSCRIPTION_NAME = os.environ.get('SUBSCRIPTION_NAME', 'sesam-client-subscriber')
+SUBSCRIPTION_MAX_SIZE = int(os.environ.get('SUBSCRIPTION_MAX_SIZE', '1_000'))
+
 log_level = logging.getLevelName(os.environ.get("LOG_LEVEL", "INFO"))
 logging.basicConfig(level=log_level)
 
-if not PROJECT_ID:
-    logging.error("Google cloud platform project id is undefined")
+LOG = logging.getLogger(__name__)
 
-logging.info("Project id: {}".format(PROJECT_ID))
-logging.info("Payload entity key: {}".format(PAYLOAD_KEY))
+if not PROJECT_ID:
+    LOG.error("Google Cloud Platform project id is undefined")
+    exit(1)
+
+LOG.info(f"project id: {PROJECT_ID}")
+LOG.info(f"payload entity key: {PAYLOAD_KEY}")
 
 if CREDENTIALS:
     with open(CREDENTIALS_PATH, "wb") as out_file:
@@ -40,6 +46,7 @@ def process(topic_name):
     """
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(PROJECT_ID, topic_name)
+    LOG.info(f'serving publisher request for topic {topic_path}')
     input_data = request.get_json()
 
     def generate():
@@ -56,23 +63,57 @@ def process(topic_name):
             output_entity['_id'] = input_entity['_id']
 
             data: str = json.dumps(input_entity[PAYLOAD_KEY] if PAYLOAD_KEY else input_entity).encode("utf-8")
-            logging.debug("data to be sent: {}".format(data))
+            LOG.debug(f"data to be sent: {data}")
             try:
                 input_entity['future'] = publisher.publish(topic_path, data=data)
                 input_entity['future'].add_done_callback(callback)
             except Exception as e:
-                logging.error(e)
+                LOG.error(e)
                 if FAIL_ON_ERROR:
                     abort(500, str(e))
-                output_entity['result'] = "ERROR: {}".format(str(e))
+                output_entity['result'] = f"ERROR: {e}"
         yield "]"
 
     return Response(generate(), content_type="application/json")
 
 
+@APP.route('/<topic_name>', methods=['GET'])
+def consume(topic_name):
+    subscriber = pubsub_v1.SubscriberClient()
+
+    sub_path = subscriber.subscription_path(PROJECT_ID, SUBSCRIPTION_NAME)
+    LOG.info(f'serving consumer request to topic {topic_name} for subscription {sub_path}')
+
+    response = subscriber.pull(sub_path, max_messages=1000, return_immediately=True)
+
+    def generate(messages):
+        first = True
+        yield '['
+
+        for msg in messages.received_messages:
+            data_item = json.loads(msg.message.data)
+            data_item['_id'] = str(msg.message.message_id)
+            data_item['_updated'] = msg.message.publish_time.seconds
+            if not first:
+                yield ','
+            else:
+                first = False
+
+            yield json.dumps(data_item)
+        yield ']'
+        LOG.debug(f'{len(messages.received_messages)}  messages processed')
+        ack_ids = [msg.ack_id for msg in messages.received_messages]
+
+        if ack_ids:
+            subscriber.acknowledge(sub_path, ack_ids)
+            LOG.debug(f'{len(ack_ids)} acknowledged')
+
+    return Response(generate(response), content_type='application/json')
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', '5000'))
-    if logging.isEnabledFor(logging.DEBUG):
+    if logging.getLogger().isEnabledFor(logging.DEBUG):
         APP.run(debug=True, host='0.0.0.0', port=port)
     else:
         import cherrypy
